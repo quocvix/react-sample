fdd237e94c981c1a84024286423a7d04b7419ead
quocvix <quocvixcode@gmail.com>
2024-06-24 11:55:34 +0700

    add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
     return (
-        <>
-            <div>helo</div>
-        </>
+        <div>
+            <div>App.tsx</div>
+            <nav className="text-blue-300 hover:text-blue-600">
+                <Link to="/home">Home</Link>
+            </nav>
+            <Outlet />
+        </div>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App.tsx';
+import NotFound from './routes/NotFound.tsx';
+import Home from './components/Home.tsx';
+import Navbar from './components/navbar/Navbar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<App />}
+                >
+                    <Route
+                        path="home"
+                        element={<Home />}
+                    />
+                </Route>
+                <Route
+                    path="/navbar"
+                    element={<Navbar />}
+                />
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
